de005f339192d68b208d6309d75aeb39f51502cf
gecko10000 <levtey@gmail.com>
2022-01-04 00:45:28 -0800

    Add completion percentage method

--- src/gecko10000/GeckoQuests/misc/Utils.java
+++ src/gecko10000/GeckoQuests/misc/Utils.java
@@ -1,5 +1,6 @@
 package gecko10000.GeckoQuests.misc;
 
+import gecko10000.GeckoQuests.objects.Quest;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
@@ -7,6 +8,15 @@ public class Utils {
 
     public static boolean isEmpty(ItemStack item) {
         return item == null || item.getType() == Material.AIR;
+    }
+
+    public static int completionPercentage(Quest quest, long progress) {
+        long amount = quest.getAmount();
+        if (progress >= amount) {
+            return 100;
+        }
+        int percentage = Math.round(100* (progress / (float) amount));
+        return Math.min(percentage, 99);
     }
 
 }
